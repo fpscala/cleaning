f36c367d5d1e915e4b90604460e8f61d30106172
Behruz <behruz.ravshonbekovich@gmail.com>
2019-05-28 09:27:39 +0500

    Removed unused codes and upgraded, added libraries

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,21 +1,11 @@
 package controllers
 
 import javax.inject._
-import play.api.mvc._
+import play.api.mvc.{BaseController, ControllerComponents}
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
