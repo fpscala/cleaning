32e0f8229f11fbcc32460e43313dd9ceb9922565
Surojiddin <isurojiddin@gmail.com>
2019-06-03 19:08:29 +0500

    addWorker added

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,6 +1,7 @@
 package controllers
 
 import java.nio.file.{Files, Path}
+import java.util.Date
 
 import akka.actor.ActorRef
 import akka.pattern.ask
@@ -10,7 +11,7 @@ import javax.inject._
 import play.api.libs.Files.TemporaryFile
 import play.api.libs.json.Json
 import play.api.mvc.{BaseController, ControllerComponents, MultipartFormData, Request}
-import protocols.WorkerProtocol.AddImage
+import protocols.WorkerProtocol.{AddImage, AddWorker, Worker}
 import views.html._
 
 import scala.concurrent.duration.DurationInt
@@ -37,6 +38,37 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents,
       val fileName = tempFile.filename
       val imgData = getBytesFromPath(tempFile.ref.path)
       (workerManager ? AddImage(fileName, imgData)).mapTo[Unit].map { _ =>
+        Ok(Json.toJson("Successfully uploaded"))
+      }
+    }.getOrElse(Future.successful(BadRequest("Error occurred. Please try again")))
+  }}
+
+  def addWorker() = Action.async(parse.multipartFormData) { implicit request: Request[MultipartFormData[TemporaryFile]] => {
+    val body = request.body.asFormUrlEncoded
+    val surname = body.get("surname").map(_.head).getOrElse("none")
+    val fristName = body.get("frist_name").map(_.head).getOrElse("none")
+    val lastName = body.get("last_name").map(_.head)
+    val address = body.get("address").map(_.head).getOrElse("none")
+    val phone = body.get("phone").map(_.head).getOrElse("none")
+    val passportSeriesAndNumber = body.get("passport_series_and_number").map(_.head).getOrElse("none")
+//    val dayGettingPassport = body.get("day_getting_passport").flatMap(_.headOption)
+    val dayGettingPassport = new Date
+    val photoName = body.get("photo_name").map(_.head).getOrElse("none")
+    val warnings = body.get("warnings").map(_.head)
+    val pensionNumber = body.get("pension_number").map(_.head.toInt).getOrElse(0)
+    val itn = body.get("itn").map(_.head.toInt).getOrElse(0)
+    val genderId = 1
+//    val birthDay = body.get("birthday").flatMap(_.headOption)
+    val birthDay = new Date
+    val birthPlace = body.get("birth_place").map(_.head).getOrElse("none")
+    val education = 1
+
+    request.body.file("attachedFile").map { tempFile =>
+      val fileName = tempFile.filename
+      val imgData = getBytesFromPath(tempFile.ref.path)
+      (workerManager ? AddWorker(Worker(None, surname, fristName, lastName, address, phone, passportSeriesAndNumber,
+        dayGettingPassport, fileName, imgData, Option(Json.toJson(warnings)), pensionNumber, itn, genderId, birthDay,
+        birthPlace, education))).mapTo[Unit].map { _ =>
         Ok(Json.toJson("Successfully uploaded"))
       }
     }.getOrElse(Future.successful(BadRequest("Error occurred. Please try again")))
