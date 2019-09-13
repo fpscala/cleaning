3742a56ab5e3e3c8f017296bf276a79275ead8ab
Maftunbek <prince777_98@mail.ru>
2019-09-13 15:44:24 +0500

    small fix update Status is order

--- app/actors/OrderManager.scala
+++ app/actors/OrderManager.scala
@@ -1,5 +1,7 @@
 package actors
 
+import java.util.Date
+
 import akka.actor.{Actor, ActorLogging}
 import akka.pattern.pipe
 import akka.util.Timeout
@@ -20,13 +22,6 @@ class OrderManager @Inject()(val environment: Environment,
 
   implicit val defaultTimeout = Timeout(60.seconds)
 
-  def updateStatusOrder(id: Option[Int], status: Int): Future[Int] = {
-    for {
-      order <- orderDao.getOrderById(id)
-      updatedOrder = order.get.copy(statusOrder = status)
-      response <- orderDao.update(updatedOrder)
-    } yield response
-  }
 
   def receive = {
     case AddOrder(order) =>
@@ -54,21 +49,49 @@ class OrderManager @Inject()(val environment: Environment,
       case Some(isOrder) =>
         Future.successful(isOrder.linkCode)
       case None =>
-        context.system.scheduler.scheduleOnce(6.seconds) {
-          orderDao.getLastOrder.map { id =>
-            println(s"id: ${id}")
-            updateStatusOrder(id, 1)
-
-          }
-        }
-        log.info("is here")
-
+        getOrderDay
         orderDao.create(orderData).map { order =>
           order
         }
     }).flatten
   }
 
+  private def updateStatusOrder(id: Option[Int], status: Int): Future[Int] = {
+    for {
+      order <- orderDao.getOrderById(id)
+      updatedOrder = order.get.copy(statusOrder = status)
+      response <- orderDao.update(updatedOrder)
+    } yield response
+  }
+
+  private def diffInSecs(date1: Long, date2: Long = new Date().getTime): Long = {
+    (date2 - date1) / 1000
+  }
+
+  private def getOrderDay = {
+    getAllOrder.map { order =>
+      order.map { data =>
+        val diffInSec = diffInSecs(data.orderDay.getTime)
+
+        if (diffInSec >= 24 * 60 * 60 && diffInSec < 36 * 60 * 60) {
+          log.info(s"1 day: ${12 * 60 * 60}  <= $diffInSec")
+          log.info(s"id: ${data.id}")
+          updateStatusOrder(data.id, 1)
+        }
+        else if (diffInSec >= 36 * 60 * 60 && diffInSec < 48 * 60 * 60) {
+          log.info(s"1,5 day: ${36 * 60 * 60}  <= $diffInSec")
+          log.info(s"id: ${data.id}")
+          updateStatusOrder(data.id, 2)
+        }
+        else if (diffInSec >= 48 * 60 * 60) {
+          log.info(s"2 day: ${48 * 60 * 60}  <= $diffInSec")
+          log.info(s"id: ${data.id}")
+          updateStatusOrder(data.id, 3)
+        }
+      }
+    }
+
+  }
 
   private def getDetails(linkCode: String) = {
     for {
